374e5de251e6854d45d2cc62dcef1d3ac203f2e0
wolf <florian.wolf@student.uva.nl>
2021-01-29 23:19:32 +0100

    Pretty plots :white_flower:

--- cluster-computing/iterations2.py
+++ cluster-computing/iterations2.py
@@ -1,23 +1,35 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from matplotlib.pyplot import figure
 
-with open('results/q2.txt') as f:
+
+with open('cluster-computing/results/q2.txt') as f:
     lines = f.readlines()[3:-1]
 
 result = [x.split(' ') for x in lines]
 itera = []
 runtime = []
 
+itera.append(0.)
+runtime.append(0.)
 for estimate in result:
-    runtime.append(estimate[4])
-    itera.append(estimate[6])
+    runtime.append(float(estimate[4]))
+    itera.append(float(estimate[6])/1000000.)
 
 
-plt.plot(itera, runtime)
-plt.xlabel("Leibniz Numbers")
-plt.xticks(rotation=90)
+figure(num=None, figsize=(6,3),)
+sns.set()
+sns.set_context("paper")
+
+plt.plot(np.array(itera), runtime, 'g', )
+plt.fill_between(np.array(itera), runtime, facecolor='green', alpha=0.1)
+plt.xlabel("Leibniz Numbers $10^6$")
 plt.ylabel("Computing Time (s)")
-plt.title("Estimation of Pi (24 threads)")
-plt.tight_layout()
-plt.savefig("Lisa_leibniz.png")
+plt.ylim(0, np.max(runtime)*11/10)
+plt.xlim(0, np.max(itera))
+plt.title("Estimation of $\pi$ (24 threads)")
+
+plt.savefig("cluster-computing/Lisa_leibniz.png", bbox_inches='tight')
 
 plt.show()
--- cluster-computing/main.py
+++ cluster-computing/main.py
@@ -1,6 +1,10 @@
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+from matplotlib.pyplot import figure
 
-with open('results/q1.txt') as f:
+
+with open('cluster-computing/results/q1.txt') as f:
     lines = f.readlines()[2:]
 
 cpus = []
@@ -14,13 +18,22 @@ for line in lines:
     else:
         runtime.append(float(line.split()[5]))
 
-print(cpus)
-print(runtime)
+print(len(cpus))
+print(np.max(cpus))
+print(len(runtime))
+print(np.max(runtime))
 
-plt.plot(cpus, runtime)
+figure(num=None, figsize=(6,3),)
+sns.set()
+sns.set_context("paper")
+
+plt.plot(np.array(cpus), runtime, 'g', )
+plt.fill_between(np.array(cpus), runtime, facecolor='green', alpha=0.1)
 plt.xlabel("Threads")
+plt.ylim(0, np.max(runtime)*11/10)
+plt.xlim(1, np.max(cpus))
 plt.ylabel("Computing Time (s)")
 plt.title(" Estimation of Pi")
-# plt.savefig("lisa_threads1-24.png")
+plt.savefig("cluster-computing/lisa_threads1-24.png", bbox_inches='tight')
 
 plt.show()
