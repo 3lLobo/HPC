7f6978d497c181d037b48ee152cf5430b7d64204
ccfelius <charlottefelius@Charlottes-MacBook-Pro.local>
2021-02-03 16:02:33 +0100

    Kind of working version

--- MPI/vector-matrix.c
+++ MPI/vector-matrix.c
@@ -1,4 +1,4 @@
-#define N 4
+#define N 160000
 #include <stdio.h>
 #include <math.h>
 #include <sys/time.h>
@@ -9,51 +9,67 @@
 
 void print_results(char *prompt, int a[N]);
 
-int multiplicate(double vec[], double M[],  int len) {
-
-	// Create resulting row
-	int res[len];
-
-	// Multiply every element of vector with corresponding matrixrow
-	for (int i = 0; i < len; i++){
-		res[i] = vec[i] * M[i];
-	}
-
-	return 1;
-}
 
 int main(int argc, char *argv[])
 {
-    int i, rank, size, cc =0;
-    int b[N]={1,2,3,4};
-    int a[N][N]={{1,1,1,2},{1,1,1,3},{1,1,1,4},{1,1,1,5}};
+    int i, rank, size, sum=0;
+    int b[N];
+    int a[N][N];
     int c[N];
-    int aa[N];
+    int *aa;
+    double start, end, final;
 
+    //Initialize simple vectors
+    for(int k=0; k < N; k++){
+	b[k] = k+1;
+	for (int r=0; r < N; r++){
+	    a[k][r] = 1;
+	}
+    }
+
+    //Initialize MPI parallel, get comm size and process rank
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+
+    // Determine size of processes
+    int local = (int)ceil((double)(N/size));
+
+    //Allocate memory for distributed vector
+    aa = (int*)malloc(N*local*sizeof(int));
+
     //scatter rows matrix to different processes     
-    MPI_Scatter(&a, N*N/size, MPI_INT, aa, N, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(&a, N*local, MPI_INT, aa, N*local, MPI_INT, 0, MPI_COMM_WORLD);
 
     //broadcast vector to all processes
     MPI_Bcast(&b, N, MPI_INT, 0, MPI_COMM_WORLD);
 
+    // Time calculations
+    start = MPI_Wtime();
+
     MPI_Barrier(MPI_COMM_WORLD);
 
     //perform vector multiplication by all processes
+     int cc[local];
+     for (i=0; i<local; i++) { 
+     sum = 0;
+     	for (int j=0; j<N; j++){ 
+        	sum += b[j]*aa[j];
+	} cc[i] = sum;
+     }  
 
-	 for (i = 0; i < N; i++){
-          	cc = cc + aa[i] * b[i];               
-	   }
-
-    MPI_Gather(&cc, N*N/size, MPI_INT, c, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
+    MPI_Gather(&cc, local, MPI_INT, c, local, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
+
+    end = MPI_Wtime();
     MPI_Finalize();
-    if (rank == 0)
-        print_results("Resulting vector = ", c);
+    if (rank == 0){
+	final = end - start;
+        printf("Processes: %2d \t Matrix size: %4d \t Time Elapsed: %f\n\n", size, N, final);
+//        print_results("Initial vector = ", b);
+//	print_results("Resulting vector = ", c);
+    }
 }
 
 void print_results(char *prompt, int a[N])
