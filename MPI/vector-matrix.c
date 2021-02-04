46ddfad3a6b86351187b14529d5adb2899f7146a
ccfelius <charlottefelius@Charlottes-MacBook-Pro.local>
2021-02-04 10:55:41 +0100

    made the MPI loop work

--- MPI/vector-matrix.c
+++ MPI/vector-matrix.c
@@ -1,4 +1,4 @@
-#define N 160000
+#define N 16000
 #include <stdio.h>
 #include <math.h>
 #include <sys/time.h>
@@ -7,23 +7,25 @@
 #include "mpi.h"
 
 
-void print_results(char *prompt, int a[N]);
+void print_vector(char *prompt, int a[N]);
+void print_matrix(char *prompt, int a[N][N]);
 
 
 int main(int argc, char *argv[])
 {
     int i, rank, size, sum=0;
-    int b[N];
-    int a[N][N];
-    int c[N];
-    int *aa;
+    int vector[N];
+    int matrix[N][N];
+    int result[N];
+    int *chunk;
     double start, end, final;
 
     //Initialize simple vectors
     for(int k=0; k < N; k++){
-	b[k] = k+1;
+	vector[k] = k+1;
 	for (int r=0; r < N; r++){
-	    a[k][r] = 1;
+	    if (r == (N-1)) {matrix[k][r] = r+k;}
+	    else { matrix[k][r] = 1; } 
 	}
     }
 
@@ -37,13 +39,13 @@ int main(int argc, char *argv[])
     int local = (int)ceil((double)(N/size));
 
     //Allocate memory for distributed vector
-    aa = (int*)malloc(N*local*sizeof(int));
+    chunk = (int*)malloc(N*local*sizeof(int));
 
     //scatter rows matrix to different processes     
-    MPI_Scatter(&a, N*local, MPI_INT, aa, N*local, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Scatter(&matrix, N*local, MPI_INT, chunk, N*local, MPI_INT, 0, MPI_COMM_WORLD);
 
     //broadcast vector to all processes
-    MPI_Bcast(&b, N, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&vector, N, MPI_INT, 0, MPI_COMM_WORLD);
 
     // Time calculations
     start = MPI_Wtime();
@@ -54,12 +56,12 @@ int main(int argc, char *argv[])
      int cc[local];
      for (i=0; i<local; i++) { 
      sum = 0;
-     	for (int j=0; j<N; j++){ 
-        	sum += b[j]*aa[j];
+         for (int j=0; j<N; j++){ 
+        	sum += vector[j]*chunk[j+(i*N)];
 	} cc[i] = sum;
      }  
 
-    MPI_Gather(&cc, local, MPI_INT, c, local, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Gather(&cc, local, MPI_INT, result, local, MPI_INT, 0, MPI_COMM_WORLD);
     MPI_Barrier(MPI_COMM_WORLD);
 
     end = MPI_Wtime();
@@ -67,12 +69,13 @@ int main(int argc, char *argv[])
     if (rank == 0){
 	final = end - start;
         printf("Processes: %2d \t Matrix size: %4d \t Time Elapsed: %f\n\n", size, N, final);
-//        print_results("Initial vector = ", b);
-//	print_results("Resulting vector = ", c);
+//        print_vector("Initial vector = ", vector);
+//	print_matrix("Initial Matrix = ", matrix);
+//	print_vector("Resulting vector = ", result);
     }
 }
 
-void print_results(char *prompt, int a[N])
+void print_vector(char *prompt, int a[N])
 {
     int i;
 
@@ -80,5 +83,17 @@ void print_results(char *prompt, int a[N])
     for (i = 0; i < N; i++) {
         printf(" %d", a[i]);
     }
-    printf ("\n\n");
+    printf ("\n");
+}
+
+void print_matrix(char *prompt, int a[N][N])
+{
+    int i, j;
+
+    printf ("\n\n%s\n", prompt);
+    for (i = 0; i < N; i++) {
+	for (j=0; j<N; j++) {
+            printf(" %d", a[i][j]);
+    } printf("\n"); }
+    printf ("\n");
 }
