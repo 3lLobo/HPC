7e15799f21d98cdee5994a67cf2ff4c7bf4a519f
3llobo <flocwolf@gmail.com>
2021-02-05 00:42:48 +0100

    ML new model :high_heel:

--- ml_assignment/cifar10_hvd_2.py
+++ ml_assignment/cifar10_hvd_2.py
@@ -39,17 +39,27 @@ print('x_train shape:', x_train.shape)
 print(x_train.shape[0], 'train samples')
 print(x_test.shape[0], 'test samples')
 
-model = Sequential()
-model.add(Conv2D(32, kernel_size=(3, 3),
-                activation='relu',
-                input_shape=input_shape))
-model.add(Conv2D(64, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(128, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(num_classes, activation='softmax'))
+# model = Sequential()
+# model.add(Conv2D(32, kernel_size=(3, 3),
+#                 activation='relu',
+#                 input_shape=input_shape))
+# model.add(Conv2D(64, (3, 3), activation='relu'))
+# model.add(MaxPooling2D(pool_size=(2, 2)))
+# model.add(Dropout(0.25))
+# model.add(Flatten())
+# model.add(Dense(128, activation='relu'))
+# model.add(Dropout(0.5))
+# model.add(Dense(num_classes, activation='softmax'))
+
+model = tf.keras.applications.EfficientNetB0(
+    include_top=True,
+    weights="imagenet",
+    input_tensor=None,
+    input_shape=None,
+    pooling=None,
+    classes=num_classes,
+    classifier_activation="softmax",
+    )
 
 # Horovod: adjust learning rate based on number of GPUs.
 opt = tf.keras.optimizers.Adadelta(1.0 * hvd.size())
