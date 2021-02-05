4e01475162787df5415e5a7a58c331d36dbc9e8a
3llobo <flocwolf@gmail.com>
2021-02-05 22:53:44 +0100

    more models

--- ml_assignment/jobs/job_ml.sh
+++ ml_assignment/jobs/job_ml.sh
@@ -1,16 +1,14 @@
 #!/bin/bash -l
-#SBATCH -J hvd_test
-#SBATCH -o hvd_test_out.txt
-#SBATCH -e hvd_test_err.txt
+#SBATCH -J hvd_$1_$2
+#SBATCH -o hvd_$1_$2_out.txt
+#SBATCH -e hvd_$1_$2_err.txt
 
 #SBATCH -t 1:00:00
 
 #SBATCH --partition shared
 
-#SBATCH --nodes 1
+#SBATCH --nodes $2
 #SBATCH --ntasks-per-node=4
-
-###SBATCH -n 2
 
 
 
@@ -21,4 +19,4 @@ module load OpenMPI/4.0.3-GCC-9.3.0
 module load Python/3.8.2-GCCcore-9.3.0
 export PYTHONPATH="~/miniconda3/bin/python"
 
-time mpirun -np 4  PYTHONPATH cifar10_hvd_effnet.py
+time mpirun -np 4  PYTHONPATH ~/cifar10_hvd_$1.py
